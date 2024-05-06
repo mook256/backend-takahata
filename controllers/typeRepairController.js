733048b1a992fad72bb56d0cc6d77c610b7a442f
Kunanya <kununyasri2545@gmail.com>
2024-05-06 11:00:08 +0700

    backend

---
+++ controllers/typeRepairController.js
@@ -0,0 +1,16 @@
+
+module.exports = async (req, res) => {
+
+    try {
+        const [results] = await conn.query('select name from type')
+
+        res.json({
+            results: results
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error'
+        })
+        console.log(error)
+    }
+}
